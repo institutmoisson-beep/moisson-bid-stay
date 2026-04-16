345ac100b6281f2924fb77d570387e08b27a1d98
institutmoisson-beep <institutmoisson@gmail.com>
2026-04-16 09:43:35 +0200

    Update base path in Vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,20 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Cette ligne permet de charger les fichiers CSS/JS correctement sur un sous-domaine Hostinger
-  base: "./",
-  
+  base: "/",
   plugins: [react()],
-  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
-  // Configuration pour Vitest (si vous utilisez des tests)
   // @ts-ignore
   test: {
     environment: "jsdom",
